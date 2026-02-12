57554ca63a1a52277d66000160ac1443a407386f
Ilgar <ilgartaghi@gmail.com>
2026-02-11 21:56:57 -0500

    renew

---
+++ Operators.js
@@ -0,0 +1,15 @@
+let x=10,y=20;
+
+//Atithmetic iperators + - / % **
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x%y);
+console.log(y/x);
+console.log(5**2);
+x++;
+console.log(x);
+y--;
+console.log(y);
+
--- jsvariablesanddatetypes.js
+++ jsvariablesanddatetypes.js
@@ -1,6 +1,40 @@
-// var x;
-// x=10;
+// //multiple line
+// var x; // declaration
+// x=10; // initilization
+
+// //single statement declaration + initilization
+// var x=10;
+
+// //single statement - multiple variables
+// var x=100, y =200;
+
+// // document.write(x);
+// // console.log(x)
+
+// //******let */
+
+// let x;
+// x="Welcome";
+//let x="wlcome";
+
+//console.log(x);
+
+// Dif var vs. let
+
+// console.log(x);
+// var x = 100;
 // console.log(x);
 
-var x=10;
-var x=100, y=200;
+// console.log(x);
+// let x = 100;
+// console.log(x);
+
+// const x=200;
+// console.log(x);
+// const y = 100;
+// console.log(y);
+
+//*******Data types */
+
+let xy="hh";
+console.log(typeof(xy));
