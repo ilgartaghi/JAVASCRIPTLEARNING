81c79912797fa2882888d13a6bfacb528a083aa7
itaghiyev <110480452+itaghiyev@users.noreply.github.com>
2026-02-13 08:38:01 -0500

    While Loops

---
+++ WhileLoop.js
@@ -0,0 +1,26 @@
+//print 1...10
+
+// let i=5
+// while(i<=10)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+//print even # to 10
+
+// let i=2
+// while(i<=10)
+// {
+//     console.log(i);
+//     i+=2; //i=i+2
+// }
+
+// desc 10 9 8 7 6 5 4 3 2 1
+
+let i=10
+while(i>0)
+{
+    console.log(i);
+    i--;
+}
---
+++ dowhileloop.js
@@ -0,0 +1,17 @@
+//1.....10
+
+// let i =100;
+// do
+// {
+// console.log(i);
+// i++
+// }while(i<=10);
+
+//10...1
+
+let i =10;
+do
+{
+console.log(i);
+i--
+}while(i>0);
